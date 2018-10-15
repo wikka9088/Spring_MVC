29ef58453cf7c5b94d390ad72159ce5553f892c0
V+ <wikka9088@gmail.com>
2018-10-14 21:29:45 -0400

    add add/show category page

---
+++ springmvc/src/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pojo.Category;
+
+@Controller
+public class CategoryController {
+	@RequestMapping("/addCategory")
+	public ModelAndView add(Category category) throws Exception {
+		ModelAndView mav = new ModelAndView("showCategory");
+		return mav;
+	}
+}
