8eb5587b585d51244022401627bb360b1645f772
V+ <wikka9088@gmail.com>
2018-10-14 21:59:41 -0400

    how to use session

---
+++ springmvc/WebContent/WEB-INF/page/session.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Session count</title>
+</head>
+<body>
+	<div>session has been visited ${count} times.</div>
+</body>
+</html>
--- springmvc/src/controller/IndexController.java
+++ springmvc/src/controller/IndexController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
@@ -20,6 +21,18 @@ public class IndexController {
 	@RequestMapping("/jump")
 	public ModelAndView jump() throws Exception {
 		ModelAndView mav = new ModelAndView("redirect:/index");
+		return mav;
+	}
+	
+	@RequestMapping("/check")
+	public ModelAndView check(HttpSession session) {
+		Integer count = (Integer) session.getAttribute("count");
+		if (count == null) {
+			count = 0;
+		}
+		count++;
+		session.setAttribute("count", count);
+		ModelAndView mav = new ModelAndView("session");
 		return mav;
 	}
 }
