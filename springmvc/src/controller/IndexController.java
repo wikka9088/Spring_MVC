0138d46441e86dd74448d7916cc282a83f5c32a8
V+ <wikka9088@gmail.com>
2018-10-14 20:44:05 -0400

    project back

---
+++ springmvc/src/controller/IndexController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("message", "Hello Spring MVC");
+		return mav;
+	}
+}
